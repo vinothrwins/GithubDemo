d2ab73e41c79c16760aa899695c16bebc48ff45a
PCNAME <PCNAME@DESKTOP-IB6GJ2Q>
2022-06-19 04:24:21 +0530

    sysout message added

--- GithubDemoProject/src/com/nxtgenai/demo/WelcomeUser.java
+++ GithubDemoProject/src/com/nxtgenai/demo/WelcomeUser.java
@@ -5,6 +5,9 @@ public class WelcomeUser {
 	public static void main(String[] args) {
 		// Code added by Vinoth R
 		System.out.println("Welcome to Github Demo Class");
+		
+		// Code added by Pranav V
+		System.out.println("Thank you for sharing the code :) ");
 
 	}
 
