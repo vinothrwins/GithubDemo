88ba06e7972710a24cabb90d745d5f2642e96053
Vinoth Rathinam <vinothrwins@gmail.com>
2022-06-19 04:18:55 +0530

    Adding new project

---
+++ GithubDemoProject/src/com/nxtgenai/demo/WelcomeUser.java
@@ -0,0 +1,11 @@
+package com.nxtgenai.demo;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+		// Code added by Vinoth R
+		System.out.println("Welcome to Github Demo Class");
+
+	}
+
+}
